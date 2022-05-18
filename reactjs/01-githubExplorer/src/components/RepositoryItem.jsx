e94f4c9b1f6e2dddc23ddf432a806aadafb648e6
Alessandro Cruz <alessandrocruzadm01@gmail.com>
2022-05-17 21:01:38 -0500

    request de repositorios

--- reactjs/01-githubExplorer/src/components/RepositoryItem.jsx
+++ reactjs/01-githubExplorer/src/components/RepositoryItem.jsx
@@ -1,10 +1,11 @@
+import './../styles/repositories.scss'
 export function RepositoryItem(props) {
     return(
         <li>
             <strong>{props.repository.name ?? 'Default'}</strong>
             <p> {props.repository.description} </p>
 
-            <a href={props.repository.link}>
+            <a href={props.repository.html_url}>
                 Acessar Repositorios
             </a>
         </li>
--- reactjs/01-githubExplorer/src/components/RepositoryList.jsx
+++ reactjs/01-githubExplorer/src/components/RepositoryList.jsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from "react"
 import { RepositoryItem } from "./RepositoryItem"
+
+
+// https://api.github.com/orgs/rocketseat/repos
 
 const rep = {
     name: 'unform',
@@ -7,18 +11,28 @@ const rep = {
 }
 
 export function RepositoryList() {
+
+    const [repositorios , setRepositorios ] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositorios(data))
+    }, [])
+
     return(
-        <section>
+        <section class="repository-list ">
             <h1>Lista de repositórios</h1>
 
             <ul>
                 <li>
-                    <RepositoryItem 
-                        repository={rep}
-                    />
-                    <RepositoryItem repository={rep}/>
-                    <RepositoryItem repository={rep}/>
-                    <RepositoryItem repository={rep}/>
+                    {repositorios.map(repositorio => {
+                        return(
+                            <RepositoryItem key={repositorio.name} repository={repositorio}/>
+                        )
+                    })}
+
+                  
                 </li>
 
             </ul>
